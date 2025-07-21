140ad42e6eee4091fb036dc3032029ddf345bd68
reyhanhmdani <rey7dan7@gmail.com>
2025-07-21 23:05:28 +0700

    tambahan animasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         green: "#c2ce3b",
         text: "#4b5563",
       },
+      animation: {
+        "bounce-slow": "bounce 3s infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+        float: "float 6s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+      },
     },
   },
 };
