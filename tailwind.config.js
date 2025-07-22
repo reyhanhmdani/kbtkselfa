a27792c408b01a7f37408160ae1d351e324e46c5
reyhanhmdani <rey7dan7@gmail.com>
2025-07-22 10:58:04 +0700

    tukar warna biru

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         primary: "#00ab66",
         yellow: "#f8f806",
         light: "#f8f8f8",
-        secondary: "#1c5fc2",
+        secondary: "#2f308a",
         thirdy: "#00c2ff",
         green: "#c2ce3b",
         text: "#4b5563",
